2af033926c3dfca8021c049d0559e5fee65a74fd
ROHANUR RAHMAN <mdrohanurrahmanontu@gmail.com>
2023-12-21 20:54:11 +0600

    solved using switch statement and put the code into try catch block to handle error

--- basic/simple_arithmetic_calculator.dart
+++ basic/simple_arithmetic_calculator.dart
@@ -1,29 +1,72 @@
 import 'dart:io';
 
 void main() {
-  double a, b, add, sub, mul, div;
+  
+  try {
+    double a, b, add, sub, mul, div;
   String c;
-  print("Enter your 1st digit = ");
-  a = double.parse(stdin.readLineSync()!);
-  print("Enter 2nd digit =");
-  b = double.parse(stdin.readLineSync()!);
-  print("choose operation + , - , x , / =");
-  c = stdin.readLineSync()!;
+    print("Enter your 1st digit = ");
+    a = double.parse(stdin.readLineSync()!);
+    print("Enter 2nd digit =");
+    b = double.parse(stdin.readLineSync()!);
+    print("choose operation + , - , x , / =");
+    c = stdin.readLineSync()!;
+     switch (c) {
+    case '+':
+      {
+        add = a + b;
+        print("Your reuslt for $a+$b= $add");
+      }
+      break;
+    case '-':
+      {
+        sub = a - b;
+        //   print("Your reuslt for $a-$b= $sub");
+      }
 
-  if (c == '+') {
-    add = a + b;
-    print("Your reuslt for $a+$b= $add");
-  } else if (c == '-') {
-    sub = a - b;
-    print("Your reuslt for $a-$b= $sub");
-  } else if (c == 'x' || c == '*') {
-    mul = a * b;
-    print("Your reuslt for $a x $b= $mul");
-  } else if (c == '/') {
-    div = a / b;
-    print("Your reuslt for $a/$b= $div");
-  } else
-    () {
-      print("Invalid input ");
-    };
+      break;
+    case '*' || 'x':
+      {
+        mul = a * b;
+        print("Your reuslt for $a x $b= $mul");
+      }
+      break;
+
+    case '/':
+      {
+        div = a / b;
+        print("Your reuslt for $a/$b= $div");
+      }
+      break;
+    default:
+      {
+        print('invalid');
+      }
+
+
+
+      
+  // if (c == '+') {
+  //   add = a + b;
+  //   print("Your reuslt for $a+$b= $add");
+  // } else if (c == '-') {
+  //   sub = a - b;
+  //   print("Your reuslt for $a-$b= $sub");
+  // } else if (c == 'x' || c == '*') {
+  //   mul = a * b;
+  //   print("Your reuslt for $a x $b= $mul");
+  // } else if (c == '/') {
+  //   div = a / b;
+  //   print("Your reuslt for $a/$b= $div");
+  // } else
+  //   () {
+  //     print("Invalid input ");
+  //   };
+  }
+  } catch (e) {
+    print(e.toString());
+  }
+
+
+ 
 }
