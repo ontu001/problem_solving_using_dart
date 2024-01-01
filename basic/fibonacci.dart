c10376a5049a7a7773636a0e1fabc7c6e6485dd3
ROHANUR RAHMAN <mdrohanurrahmanontu@gmail.com>
2024-01-01 14:45:33 +0600

    fibonacci solved

--- basic/fibonacci.dart
+++ basic/fibonacci.dart
@@ -1,3 +1,18 @@
-void main(){
-  
+import 'dart:io';
+
+void main() {
+  try {
+    int first = 0, second = 1, n, fibo = 0;
+    print("Enter your fibonacci count =");
+    n = int.parse(stdin.readLineSync()!);
+    for (int count = 0; count < n; count++) {
+      fibo = first + second;
+      first = second;
+      second = fibo;
+    }
+    print('fibo is = $fibo');
+  } catch (e) {
+    
+    print('Invalid Input ');
+  }
 }
